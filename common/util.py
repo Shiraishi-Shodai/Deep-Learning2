04ac66ba9c5d46a3dae191c82e9090e69c1b7930
Shiraishi-Shodai <siranosuke1227@gmail.com>
2025-08-25 01:10:21 +0900

    vector first

--- common/util.py
+++ common/util.py
@@ -3,38 +3,116 @@ import sys
 sys.path.append('..')
 import os
 from common.np import *
+import numpy as np
+
+
+# def preprocess(text):
+#     text = text.lower()
+#     text = text.replace('.', ' .')
+#     words = text.split(' ')
+
+#     word_to_id = {}
+#     id_to_word = {}
+#     for word in words:
+#         if word not in word_to_id:
+#             new_id = len(word_to_id)
+#             word_to_id[word] = new_id
+#             id_to_word[new_id] = word
+
+#     corpus = np.array([word_to_id[w] for w in words])
+
+#     return corpus, word_to_id, id_to_word
 
 
 def preprocess(text):
+    """
+    単語に分割
+    """
     text = text.lower()
-    text = text.replace('.', ' .')
-    words = text.split(' ')
+    text = text.replace(".", " .") # 単語を分割するために、ピリオドの前にスペースを追加
+    words = text.split(" ") # 各単語を分割
 
+    """
+    単語にIDを振る
+    """
     word_to_id = {}
     id_to_word = {}
+
     for word in words:
-        if word not in word_to_id:
-            new_id = len(word_to_id)
+        if word not in word_to_id.keys():
+            new_id = len(word_to_id.keys())
             word_to_id[word] = new_id
             id_to_word[new_id] = word
 
+    """
+    単語のidリストを取得
+    """
+    # a = np.array([x for x in id_to_word.keys()]) # [0 1 2 3 4 5 6]
+    # b = np.array([word_to_id[w] for w in words]) # [0 1 2 3 4 1 5 6]
+    # print(a, b)
     corpus = np.array([word_to_id[w] for w in words])
-
+    
     return corpus, word_to_id, id_to_word
 
 
+# def cos_similarity(x, y, eps=1e-8):
+#     '''コサイン類似度の算出
+
+#     :param x: ベクトル
+#     :param y: ベクトル
+#     :param eps: ”0割り”防止のための微小値
+#     :return:
+#     '''
+#     nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+#     ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+#     return np.dot(nx, ny)
+
 def cos_similarity(x, y, eps=1e-8):
     '''コサイン類似度の算出
-
     :param x: ベクトル
     :param y: ベクトル
     :param eps: ”0割り”防止のための微小値
-    :return:
+    :return
     '''
-    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
-    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+
+    nx = x / (np.sqrt(np.sum(x**2)) + eps) # xの正規化
+    ny = y / (np.sqrt(np.sum(y**2)) + eps)# yの正規化
+
     return np.dot(nx, ny)
 
+
+# def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+#     '''類似単語の検索
+
+#     :param query: クエリ（テキスト）
+#     :param word_to_id: 単語から単語IDへのディクショナリ
+#     :param id_to_word: 単語IDから単語へのディクショナリ
+#     :param word_matrix: 単語ベクトルをまとめた行列。各行に対応する単語のベクトルが格納されていることを想定する
+#     :param top: 上位何位まで表示するか
+#     '''
+#     if query not in word_to_id:
+#         print('%s is not found' % query)
+#         return
+
+#     print('\n[query] ' + query)
+#     query_id = word_to_id[query]
+#     query_vec = word_matrix[query_id]
+
+#     vocab_size = len(id_to_word)
+
+#     similarity = np.zeros(vocab_size)
+#     for i in range(vocab_size):
+#         similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+#     count = 0
+#     for i in (-1 * similarity).argsort():
+#         if id_to_word[i] == query:
+#             continue
+#         print(' %s: %s' % (id_to_word[i], similarity[i]))
+
+#         count += 1
+#         if count >= top:
+#             return
 
 def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
     '''類似単語の検索
@@ -45,25 +123,29 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
     :param word_matrix: 単語ベクトルをまとめた行列。各行に対応する単語のベクトルが格納されていることを想定する
     :param top: 上位何位まで表示するか
     '''
-    if query not in word_to_id:
-        print('%s is not found' % query)
-        return
 
-    print('\n[query] ' + query)
+    if query not in word_to_id.keys():
+        print(f"{query} is not found")
+        return
+    
+    print("\n[query]" + query)
     query_id = word_to_id[query]
     query_vec = word_matrix[query_id]
 
-    vocab_size = len(id_to_word)
+    vocab_size =len(word_to_id)
 
     similarity = np.zeros(vocab_size)
-    for i in range(vocab_size):
-        similarity[i] = cos_similarity(word_matrix[i], query_vec)
 
+    for i in range(vocab_size): 
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+    
     count = 0
-    for i in (-1 * similarity).argsort():
+
+    for i in np.argsort(-1 * similarity): # 降順に並べ替えてインデックスを返すnp.argsort
         if id_to_word[i] == query:
             continue
-        print(' %s: %s' % (id_to_word[i], similarity[i]))
+        
+        print(f"{id_to_word[i]}: {similarity[i]}")
 
         count += 1
         if count >= top:
@@ -94,14 +176,40 @@ def convert_one_hot(corpus, vocab_size):
     return one_hot
 
 
-def create_co_matrix(corpus, vocab_size, window_size=1):
-    '''共起行列の作成
+# def create_co_matrix(corpus, vocab_size, window_size=1):
+#     '''共起行列の作成
 
-    :param corpus: コーパス（単語IDのリスト）
-    :param vocab_size:語彙数
-    :param window_size:ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
-    :return: 共起行列
-    '''
+#     :param corpus: コーパス（単語IDのリスト）
+#     :param vocab_size:語彙数
+#     :param window_size:ウィンドウサイズ（ウィンドウサイズが1のときは、単語の左右1単語がコンテキスト）
+#     :return: 共起行列
+#     '''
+#     corpus_size = len(corpus)
+#     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+#     for idx, word_id in enumerate(corpus):
+#         for i in range(1, window_size + 1):
+#             left_idx = idx - i
+#             right_idx = idx + i
+
+#             if left_idx >= 0:
+#                 left_word_id = corpus[left_idx]
+#                 co_matrix[word_id, left_word_id] += 1
+
+#             if right_idx < corpus_size:
+#                 right_word_id = corpus[right_idx]
+#                 co_matrix[word_id, right_word_id] += 1
+
+#     return co_matrix
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    """共起行列の作成
+    :param corpus: コーパス(単語のIDリスト)
+    :param vocab_size: 語彙数
+    :param window_size: 左右のコンテキストの大きさ
+    :return 共起行列
+    """
+
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
 
@@ -116,12 +224,10 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
 
             if right_idx < corpus_size:
                 right_word_id = corpus[right_idx]
-                co_matrix[word_id, right_word_id] += 1
-
+                co_matrix[word_id, right_word_id] =+ 1
     return co_matrix
 
-
-def ppmi(C, verbose=False, eps = 1e-8):
+# def ppmi(C, verbose=False, eps = 1e-8):
     '''PPMI（正の相互情報量）の作成
 
     :param C: 共起行列
@@ -143,6 +249,29 @@ def ppmi(C, verbose=False, eps = 1e-8):
                 cnt += 1
                 if cnt % (total//100 + 1) == 0:
                     print('%.1f%% done' % (100*cnt/total))
+    return M
+
+def ppmi(C, verbose=False, eps = 1e-8):
+    '''PPMI（正の相互情報量）の作成
+    :param C: 共起行列
+    :param verbose: 進行状況を出力するかどうか
+    :return:
+    '''
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C) # スカラー(全要素の合計)
+    S = np.sum(C, axis=0) # ベクトル(行方向に合計) 各単語が別の単語と共起した回数
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[i] * S[j]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total // 100 + 1) == 0:
+                    print(f"{100*cnt/total:.1f} done")
     return M
 
 
